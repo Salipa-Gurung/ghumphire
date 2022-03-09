824cf7e27157ee56c8a37effdd41721274020ecd
Salipa-Gurung <salipagurung@gmail.com>
2022-03-09 11:07:52 +0545

    add template

--- ghumphire/routes/index.js
+++ ghumphire/routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/home', function(req, res, next) {
+  res.render('homepage')
+});
+
+router.get('/reviewer', function(req, res, next) {
+
+});
+
+router.get('/author', function(req, res, next) {
+
+});
+
+
 module.exports = router;
